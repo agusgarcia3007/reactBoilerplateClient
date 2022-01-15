bc256f6c9ab21e5f9c4db5cd4d1de5334acdf440
root <root@agustinPC.localdomain>
2022-01-15 18:17:43 -0300

    changed url for axios baseURL

--- src/config/tokenAuth.js
+++ src/config/tokenAuth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.REACT_BACK_URL,
 });
 
 const tokenAuth = (token) => {
